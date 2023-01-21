1c3bcd39b3db080682207f2755ab274a0f6167b1
Terx37 <jenik.marszalek@gmail.com>
2023-01-21 22:51:02 +0100

    initial commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,28 @@
+<template >
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    computed: {
+      username() {
+        // We will see what `params` is shortly
+        return this.$route.params.username
+      },
+    },
+    methods: {
+      goToDashboard() {
+        let isAuthenticated=true
+        if (isAuthenticated) {
+          this.$router.push('/dashboard')
+        } else {
+          this.$router.push('/login')
+        }
+      },
+    },
+  }
+</script>
+<style lang="">
+    
+</style>
